521fbf0b2ad84a2b07c9da667fa52c96aaa2560e
20161313 <dbstjd427@gmail.com>
2020-09-22 08:51:24 +0900

    20200921 vue study

---
+++ practice-of-vue/src/components/User.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="blue lighten-3 pa-3">
+    <h1>User Components</h1>
+    <p>Name: {{name}}</p>
+
+    <v-btn color="success" @click="changeName()">ChangeName</v-btn>
+    <br />
+    <br />
+    <v-layout row wrap>
+      <v-flex xs12 sm6>
+        <UserDetail :nameOfChild="name"></UserDetail>
+      </v-flex>
+      <v-flex xs12 sm6>
+        <UserEdit></UserEdit>
+      </v-flex>
+    </v-layout>
+  </div>
+</template>
+
+<script>
+import UserDetail from "./UserDetail.vue"
+import UserEdit from "./UserEdit.vue"
+
+export default {
+  components: {
+    UserDetail,
+    UserEdit
+  },
+  data () {
+    return {
+      name: "Vue.js"
+    }
+  },
+  methods: {
+    changeName () {
+      this.name = "YoonCastle"
+    }
+  }
+
+
+
+}
+</script>
---
+++ practice-of-vue/src/components/UserDetail.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="red lighten-3 pa-3">
+    <h3>User Info</h3>
+    <p>{{nameOfChild}}</p>
+    <p>{{sayHello}}</p>
+    <v-btn color="primary" @click="switchName()">ChangeName</v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    nameOfChild: {
+      type: String,
+      default: "LEGO"
+    }
+  },
+  computed: {
+    sayHello () {
+      return this.nameOfChild + ' Hello'
+    }
+  },
+  methods: {
+    switchName () {
+      this.nameOfChild = "Hwang"
+    }
+  }
+
+}
+</script>
